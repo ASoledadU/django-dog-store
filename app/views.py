86305f3858b04c84e2610023071f1cfc94cf9144
Student <student@donation.local>
2019-11-15 13:22:22 -0600

    Changed the location of the redirect if a product is out of stock: ___________________________________________________________________ Because I decided to add the feature to include the owners phone number to the dog tag I wanted to make sure all my past code still passed the tests. I found a bug in my previous code that I accidently used the template name to redirect rather than the name of the path. Whoops.

--- app/views.py
+++ app/views.py
@@ -28,7 +28,7 @@ def purchase_dog_product(request, dog_product_id):
         return redirect("purchase_detail", purchase.id)
     elif dog_product.quantity == 0:
         messages.error(request, f"{dog_product.name} is out of stock")
-        return redirect("dog_product_detail.html", dog_product_id)
+        return redirect("dog_product_detail", dog_product_id)
 
 
 def purchase_detail(request, purchase_id):
@@ -45,8 +45,9 @@ def new_dog_tag(request):
             owner_name = form.cleaned_data["owner_name"]
             dog_name = form.cleaned_data["dog_name"]
             dog_birthday = form.cleaned_data["dog_birthday"]
+            owner_phone = form.cleaned_data["owner_phone"]
             DogTag.objects.create(
-                owner_name=owner_name, dog_name=dog_name, dog_birthday=dog_birthday
+                owner_name=owner_name, dog_name=dog_name, dog_birthday=dog_birthday, owner_phone=owner_phone
             )
             return redirect("dog_tag_list")
         else:
