d410c22a956259483a8e38806b05c6774fce3843
Student <student@donation.local>
2019-11-15 09:54:29 -0600

    Had to delete and rewrite this code to get it to work. Not sure why this was they case but it was the only solution that worked.

--- dog_store/urls.py
+++ dog_store/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
         app.views.purchase_dog_product,
         name="purchase_dog_product",
     ),
-    path("purchase_dog_product", app.views.purchase_detail, name="purchase_detail"),
+    path("purchase/<purchase_id>", app.views.purchase_detail, name="purchase_detail"),
     path("dogtag/new", app.views.new_dog_tag, name="new_dog_tag"),
     path("dogtag", app.views.dog_tag_list, name="dog_tag_list"),
 ]
